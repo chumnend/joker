51bada4f936be01096fd4e983c583a8dc0ffd297
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-11-24 22:04:56 -0500

    added joke routes

--- joker/webapp.py
+++ joker/webapp.py
@@ -2,12 +2,21 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 
-def hello_world(request):
-  return Response('<p>Hello World</p>')
+def list_jokes(request):
+  return Response('List of jokes\n')
+
+def random_joke(request):
+    return Response('Knock Knock\n')
 
 if __name__ == '__main__':
   config = Configurator()
-  config.add_view(hello_world)
+
+  config.add_route('jokes', '/jokes')
+  config.add_route('joke', '/joke')
+
+  config.add_view(list_jokes, route_name='jokes')
+  config.add_view(random_joke, route_name='joke')
+
   app = config.make_wsgi_app()
   server = make_server('0.0.0.0', 8000, app)
   server.serve_forever()
