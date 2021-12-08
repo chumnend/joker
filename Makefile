7e9d27afd92c5b6cb4e43863a7f4454aa4d3d0e7
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-12-07 22:37:58 -0500

    removed statics and templates from cookiecutter

--- Makefile
+++ Makefile
@@ -3,8 +3,12 @@ all: install-deps
 .PHONY: install-deps
 install-deps:
 	@pip install --upgrade pip setuptools
-	@pip install -e .
+	@pip install -e ".[testing]"
 
 .PHONY: start
 start:
 	@pserve development.ini --reload
+
+.PHONY: test
+test:
+	@pytest
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,13 +1,6 @@
-from joker.views.default import my_view
-from joker.views.notfound import notfound_view
+from joker.views.hello import hello_world
 
 
-def test_my_view(app_request):
-    info = my_view(app_request)
+def test_hello_world_view(app_request):
+    info = hello_world(app_request)
     assert app_request.response.status_int == 200
-    assert info['project'] == 'joker'
-
-def test_notfound_view(app_request):
-    info = notfound_view(app_request)
-    assert app_request.response.status_int == 404
-    assert info == {}
